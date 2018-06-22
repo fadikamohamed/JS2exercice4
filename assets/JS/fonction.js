14616bbc47ac4f81131bcaa7ffb72585e85bdec2
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-22 14:30:00 +0200

    Exercice 4 terminé

---
+++ assets/JS/fonction.js
@@ -0,0 +1,5 @@
+function resetvalue() {
+  var lastnamevalue = document.getElementById('lastname').value = "";
+  var firstname = document.getElementById('firstname').value = "";
+  var city = document.getElementById('city').value = "";
+}
