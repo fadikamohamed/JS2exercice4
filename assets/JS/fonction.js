f53d492a0fd1e92cf75f259cba964c4f4e0f1d4d
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-23 11:18:30 +0200

    Ajout modif addEventListener

--- assets/JS/fonction.js
+++ assets/JS/fonction.js
@@ -1,3 +1,6 @@
+resetval = document.querySelector('button');
+resetval.addEventListener('click',resetvalue);
+
 function resetvalue() {
   var lastnamevalue = document.getElementById('lastname').value = "";
   var firstname = document.getElementById('firstname').value = "";
